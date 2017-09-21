63f47156161858773d889210f5026339252bd27c
Andriy Hrynyak <anh@conscensia.com>
2017-09-21 10:04:12 +0300

    --added missing files

---
+++ SolarMonitor/SolarMonitor.Communication.Test/Messages/Queries/QpiriMessageTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolarMonitor.Communication.Messages.Queries;
+
+namespace SolarMonitor.Communication.Test.Messages.Queries
+{
+    [TestClass]
+    public class QpiriMessageTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var target = new QpiriMessage();
+            target.SetResponse(new List<string>() {"2365","256","2305","500","510","2563","6325"});
+
+        }
+    }
+}
---
+++ SolarMonitor/SolarMonitor.Communication/Messages/Attributes/FieldBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarMonitor.Communication.Messages.Attributes
+{
+    public class FieldBaseAttribute : Attribute
+    {
+        public FieldBaseAttribute(int position)
+        {
+            Position = position;
+        }
+
+        public int Position { get; set; }
+    }
+}
